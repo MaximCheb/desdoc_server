2f53fef377642e63fc6b56311d51da4a25151d97
MaximCheb <chebola.maxim@gmail.com>
2022-11-09 02:18:30 +0300

    rename how other classes in project_involve service

--- src/main/java/com/doc/des/server/controller/ProjectInvolveController.java
+++ src/main/java/com/doc/des/server/controller/ProjectInvolveController.java
@@ -1,8 +1,10 @@
 package com.doc.des.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,9 @@ import com.doc.des.server.entity.ProjectInvolveEntity;
 import com.doc.des.server.service.ProjectInvolvedService;
 
 @RestController
-@RequestMapping("/involve")
-public class InvolvedProjectController {
-	
+@RequestMapping("/involve_project")
+public class ProjectInvolveController {
+@Autowired	
 private ProjectInvolvedService projectInvolveService;
 	
 	@GetMapping("/all")
@@ -23,28 +25,35 @@ private ProjectInvolvedService projectInvolveService;
 		try {
 			return ResponseEntity.ok("Server work");
 		}catch(Exception e) {
+			return ResponseEntity.badRequest().body("Empty");
+		}
+	}
+	
+	@GetMapping("/id={id}")
+	public ResponseEntity getOne(@PathVariable long id) {
+		try {
+			return ResponseEntity.ok(projectInvolveService.getOne(id));
+		}catch(Exception e) {
+			return ResponseEntity.badRequest().body("No item");
+		}
+	}
+	
+	@GetMapping("/allByProject={}")
+	public ResponseEntity getByProject(@PathVariable long id) {
+		try {
+			return ResponseEntity.ok(projectInvolveService.getByProject(id));
+		}catch(Exception e) {
 			return ResponseEntity.badRequest().body("Fire? fire, Its fiasco");
 		}
 	}
-	
-	@GetMapping()
-	public ResponseEntity getOne(@RequestParam int id) {
-		try {
-			return ResponseEntity.ok("");
-		}catch(Exception e) {
-			return ResponseEntity.badRequest().body("No smartphone");
-		}
-	}
-	
-	@GetMapping("/project")
-	public ResponseEntity getByProject() {
-		try {
-			return ResponseEntity.ok("Server work");
-		}catch(Exception e) {
-			return ResponseEntity.badRequest().body("Fire? fire, Its fiasco");
-		}
-	}
-	
+	@GetMapping("/allByUser={}")
+    public ResponseEntity getByUser(@PathVariable long id) {
+        try {
+            return ResponseEntity.ok(projectInvolveService.getByUserId(id));
+        }catch(Exception e) {
+            return ResponseEntity.badRequest().body("Fire? fire, Its fiasco");
+        }
+    }
 	@PostMapping(path = "/new", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity newProject(@RequestBody ProjectInvolveEntity newProject) {
     	try {
