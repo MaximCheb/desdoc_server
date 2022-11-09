62eae7e9b68f02252de9bbe84aa86409b514098e
MaximCheb <chebola.maxim@gmail.com>
2022-11-09 03:43:59 +0300

    change apache poi doc creator

--- src/main/java/com/doc/des/server/doc/controller/PoiController.java
+++ src/main/java/com/doc/des/server/doc/controller/PoiController.java
@@ -1,10 +1,13 @@
 package com.doc.des.server.doc.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.doc.des.server.doc.service.PoiService;
 import com.doc.des.server.exception.AlreadyExistException;
+import com.doc.des.server.file.entity.ProjectFileSingleEntity;
+import com.doc.des.server.request.LoginRequest;
 
 @RestController
-@RequestMapping("/poi")
+@RequestMapping("/project_file")
 public class PoiController {
 	@Autowired
 	private PoiService poiService;
@@ -28,6 +33,7 @@ public class PoiController {
 //		return itemNotebooks;
 //	}
 	@GetMapping("/test")
+	@PreAuthorize("hasAuthority('manager')")
 	public ResponseEntity getOne(@RequestParam Long id) {
 		try {
 			return ResponseEntity.ok(0);
@@ -35,6 +41,15 @@ public class PoiController {
 			return ResponseEntity.badRequest().body("Not poi");
 		}
 	}
+	@GetMapping("/simple")
+	@PreAuthorize("hasAuthority('manager')")
+	public ResponseEntity createSimple(@Valid @RequestBody ProjectFileSingleEntity simpleEntity) {
+        try {
+            return ResponseEntity.ok(0);
+        }catch(Exception e) {
+            return ResponseEntity.badRequest().body("Not poi");
+        }
+    }
 //	@PostMapping(path = "/new", produces = MediaType.APPLICATION_JSON_VALUE)
 //    public ResponseEntity newProject(@RequestBody ProjectDocument newProject) {
 //    	try {
--- src/main/java/com/doc/des/server/doc/service/PoiService.java
+++ src/main/java/com/doc/des/server/doc/service/PoiService.java
@@ -2,20 +2,34 @@ package com.doc.des.server.doc.service;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.doc.des.server.file.entity.ProjectFileSingleEntity;
 
 @Service
 public class PoiService {
-public boolean createDocument() {
-	//File file = new File("C:/username/document.docx");
-//	FileInputStream fis = new FileInputStream(file.getAbsolutePath());
-//	XWPFDocument document = new XWPFDocument(fis); // Вот и объект описанного нами класса
-//	String documentLine = document.getDocument().toString(); 
-//	document.getParagraphs();
-//	XWPFParagraph lastParagraph = document.createParagraph();
-	return true;
-}
+    @Value("${desdoc.app.storage}")
+    private String storageLocation;
+    private Path storageFolder;   
+    
+    public boolean createDocumentByFile(ProjectFileSingleEntity inputFileEntity) throws FileNotFoundException {
+        storageFolder = Paths.get(storageLocation);
+        File file = new File("tmp.doc");
+        InputStream fis = new FileInputStream(file);
+        XWPFDocument document = new XWPFDocument();         
+    	return true;
+    }
+    
+//    private File getFileByPath(String fileName) {
+//        
+//    }
+
 }
